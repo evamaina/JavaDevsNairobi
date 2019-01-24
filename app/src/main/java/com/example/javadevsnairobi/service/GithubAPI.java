7d96932b9dd43a054e1698cf816f51ab25764b03
Eva Maina <evajohnson714@gmail.com>
2019-01-24 15:26:42 +0300

    ft(Github api):define the api endpoints (#9)
    - updare readme file
    [Finishes #155903085]

--- app/src/main/java/com/example/javadevsnairobi/service/GithubAPI.java
+++ app/src/main/java/com/example/javadevsnairobi/service/GithubAPI.java
@@ -1,13 +1,20 @@
 package com.example.javadevsnairobi.service;
+import com.example.javadevsnairobi.models.GithubUser;
 import com.example.javadevsnairobi.models.GithubUsersResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface GithubAPI {
 
     @GET("search/users?q=language:java+location:nairobi")
     Call<GithubUsersResponse> users();
 
+    @GET("users/{username}")
+    Call<GithubUser> getUser(@Path("username") String username);
+
 
 }
+
+
