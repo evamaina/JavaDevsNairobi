64b1d1f72515cdea32945bc3f30041aed5ebcefe
Eva Maina <evajohnson714@gmail.com>
2019-01-24 09:17:27 +0300

    ft(launch DetailActivity):Launch the DetailActivity from the MainActivity (#6)
    - implement back button functionality
    [Finishes #155820624]

--- app/src/main/java/com/example/javadevsnairobi/DetailActivity.java
+++ app/src/main/java/com/example/javadevsnairobi/DetailActivity.java
@@ -1,27 +1,47 @@
 package com.example.javadevsnairobi;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar_detail);
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View view = inflater.inflate(R.layout.bar,toolbar, false);
-        toolbar.addView(view);
+        Toolbar toolbar = findViewById(R.id.toolbar_detail);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("User detail");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
